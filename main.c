e45f3b47ecacc0c6355ac2646c9cc2967e924eb1
FJJI <fjjimenez@miuandes.cl>
2019-10-17 03:48:10 -0400

    puse consejos y coloque el set entry como en los dibujos de la guia

--- main.c
+++ main.c
@@ -21,18 +21,28 @@ int BLACK = 30, RED = 31, GREEN = 32, YELLOW = 33, BLUE = 34, MAGENTA = 35, PURP
 int* free_frames;   // array con índices de marcos libres
 struct disk* disk;
 char* BUFFER;   // string donde se escribe-lee al disco.
-unsigned int*  tabla_marcos; 
+unsigned int*  tabla_marcos;  //Segun Consejos
 char* msj;  // auxiliar
 int en_memoria; //auxiliar que marca si estamos en memoria o no
+
+int npages;
+int nframes;
+char* virtmem;
+char* physmem;
+// [FJJI] subi las declaraciones por que nos pueden servir para  la funcion a continuacion
 
 void page_fault_handler( struct page_table* pt, int page )
 {   // SE GATILLA AL QUERER ACCEDER A UNA PÁGINA QUE NO ESTÁ EN MEMORIA VIRTUAL (pt->virtmem) Y HAY QUE TRAERLA DESDE EL DISCO (disk)
 	sprintf(msj, "page fault on page #%d\n", page);
 	printcolor(RED, msj);
-	// page_table_get_entry(pt, (PAGE_SIZE*page)/BLOCK_SIZE, free_frames[page], en_memoria);
+	int frame, bits;
+	page_table_get_entry(pt, page, &frame, &bits); // Segun consejos --no se cae 
 //[FJJI]Habria que setear en (pt, page, -algo- (posiblemente su frame actual) , Prot_Read|Prot_write  (visto por los bits de proteccion, como los tomo?)  ) ,de ahi hacer un disk_read que esta abajo
 	BUFFER = malloc(sizeof(char)*40);
 	strcpy(BUFFER, "");
+	int the_frame= &frame;
+	//disk_read(disk, (PAGE_SIZE*page)/BLOCK_SIZE, &physmem[the_frame * nframes]); 
+		// [FJJI] este debiese tener formato correcto pero tira error "bad adressing"
 	disk_read(disk, (PAGE_SIZE*page)/BLOCK_SIZE, BUFFER);   // verificar segundo arg
 	    // [??] Cómo sé cuál bloque del disco leer? Cómo obtengo el bloque en el que está la página "page"?
 		// [FJJI]BUFER segun lo que leo debe ser &physmem[n°frame * frame_size]
@@ -92,8 +102,8 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 	
-	char* virtmem = page_table_get_virtmem(pt);
-	char* physmem = page_table_get_physmem(pt);
+	virtmem = page_table_get_virtmem(pt);
+	physmem = page_table_get_physmem(pt);
 	if (! strcmp(pattern, "pattern1"))
 	{
 		access_pattern1(virtmem, npages*PAGE_SIZE);
